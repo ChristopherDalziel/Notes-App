0009de3cb0cc3866c4c5946d3ee30c97a812b474
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-29 22:38:59 +1000

    ran script

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const fs = require("fs");
 
 fs.writeFileSync("notes.txt", "This file was written by NodeJS");
 
-fs.appendFileSync("notes.txt", ", Some extra data has been written by NodeJS");
+fs.appendFileSync("notes.txt", ", Some extra data has been written by NodeJS.");
