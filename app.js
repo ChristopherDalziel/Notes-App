048bc274d9d01e2d4c547805c7647ba553c7339c
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-30 23:02:13 +1000

    Added Chalk and created a bold inversed green string message printing to the console

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Each file within a NODE application have their own scope, you cannot access a variable that is written within another file unless you explicitly export it from it's own file and require it where you want to use it.
 
+const chalk = require("chalk");
 const validator = require("validator");
 const add = require("./utils.js");
 const getNotes = require("./notes.js");
@@ -9,3 +10,5 @@ const sum = add(1, 2);
 console.log(getNotes());
 
 console.log(validator.isEmail("Christopher_Dalziel@hotmail.com"));
+
+console.log(chalk.green.bold.inverse("Success!"));
