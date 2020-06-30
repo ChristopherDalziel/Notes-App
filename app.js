91637fc0cacbe0b096d9e764679c09b45dfea06f
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-30 22:56:02 +1000

    required our first npm libary, tested it's functionality

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // Each file within a NODE application have their own scope, you cannot access a variable that is written within another file unless you explicitly export it from it's own file and require it where you want to use it.
 
+const validator = require("validator");
 const add = require("./utils.js");
 const getNotes = require("./notes.js");
 
 const sum = add(1, 2);
 
 console.log(getNotes());
+
+console.log(validator.isEmail("Christopher_Dalziel@hotmail.com"));
